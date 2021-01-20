c2bc7e6d02fddf26e7e1118c8cd4691f16428cf3
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-01-20 11:06:52 -0600

    Modelo\Presiones new !

---
+++ app/modelos/Presiones.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace App\modelos;
+
+use App\clases\Modelo;
+use App\interfaces\Database;
+
+class Presiones extends Modelo
+{
+    public function __construct(Database $coneccion)
+    {
+        $tabla = 'presiones';
+        $relaciones = [
+            'menus' => "{$tabla}.usuario_id"
+
+        ]; 
+        $columnas = [
+            'unicas' => [],
+            'obligatorias' => ['sistolica','diastolica','bpm','fecha','hora'],
+            'protegidas' => []
+        ];
+        parent::__construct($coneccion, $tabla, $relaciones, $columnas);
+    }
+}
