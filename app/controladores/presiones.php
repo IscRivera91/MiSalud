e6cfa2034d15f45dd6da9a384cb2fddf3f2e9e1f
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-01-20 11:35:08 -0600

    Controlador\Presiones new

---
+++ app/controladores/presiones.php
@@ -0,0 +1,49 @@
+<?php 
+
+namespace App\controladores;
+
+use App\ayudas\Html;
+use App\clases\Controlador;
+use App\interfaces\Database;
+use App\modelos\Presiones AS ModeloPresiones;
+
+class presiones extends Controlador
+{
+    private $Presiones;
+
+    public function __construct(Database $coneccion)
+    {
+        $this->Presiones = new ModeloPresiones($coneccion);
+        $this->modelo = $this->Presiones;
+        $this->nombreMenu = 'presiones';
+        $this->breadcrumb = false;
+
+        $this->camposLista = [
+            'Id' => 'presiones_id',
+            'Usuario' => 'usuarios_nombre_completo',
+            'Bpm' => 'presiones_bpm',
+            'Activo' => 'presiones_activo'
+        ];
+        
+        parent::__construct();
+    }
+    
+    public function generaInputFiltros (array $datosFiltros): void 
+    {
+        $col = 3;
+        $this->sizeColumnasInputsFiltros = $col;
+        
+        //values de todos los inputs vacios
+        $datos['usuarios+nombre_completo'] = '';
+
+        foreach ($datosFiltros as $key => $filtro) {
+            $datos[$key] = $filtro;
+        }
+
+        $tablaCampo = 'usuarios+nombre_completo';
+        $placeholder = '';
+
+        $this->htmlInputFiltros[$tablaCampo] = Html::inputText($col,'Usuario',1,$tablaCampo,$placeholder,$datos[$tablaCampo]);
+    }
+
+}
